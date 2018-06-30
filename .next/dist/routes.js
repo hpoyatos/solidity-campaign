8eda8790a83b6f42fa710d7ebfe2f7493993afe5
Henrique Poyatos <hpoyatos@yahoo.com.br>
2018-06-30 20:17:29 -0300

    gitignore

--- .next/dist/routes.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var routes = require('next-routes')();
-
-module.exports = routes;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUFPLEFBQVAsVUFBaUIsQUFBakIiLCJmaWxlIjoicm91dGVzLmpzIiwic291cmNlUm9vdCI6IkM6L1VzZXJzL2hwb3lhL0RvY3VtZW50cy9TbWFydENvbnRyYWN0UHJvamVjdHMvc29saWRpdHktY2FtcGFpZ24ifQ==
